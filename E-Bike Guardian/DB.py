57611f0dba05af553486aab7bc154e828fade2bd
Ivailo1st <eaaivge@students.eaaa.dk>
2019-10-30 14:24:43 +0100

    Updated with Comments

--- E-Bike Guardian/DB.py
+++ E-Bike Guardian/DB.py
@@ -1,3 +1,8 @@
+
+# Imported standard Flask libraries and a few special like SQLAlchemy that is for databases
+# and flask_login which serves as the user management side of the website that communicates
+# with the the database
+
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text, update
@@ -11,16 +16,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 
-
+# This is the connection to the database its simple to understand
+# its the ip user and password plus secret _key for security reasons
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Georgiev@35.233.82.53/postgres'
 db = SQLAlchemy(app)
 app.secret_key = "ux&c[-{X'6Cc%Wh"
 
+# Here I inistialize the user manager so that I can work with it
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+# This part is where I create a class that corresponds to the table I have in my
+# database so that python can easily access the things I need from the DB
 
+# This is the bike table where the coordinates and names of the bikes are stored
 class Bike(db.Model):
     __tablename__ = "data"
     name = db.Column(db.Text)
@@ -37,7 +47,7 @@ class Bike(db.Model):
         self.longitude= longitude
 
 
-
+# This is the users tables. It contains the users username and password
 class Users(db.Model):
     __tablename__ = "users"
     username = db.Column(db.Text, primary_key=True)
@@ -49,6 +59,8 @@ class Users(db.Model):
         self.password = password
         self.role=role
 
+# This is the rent bike table. It contains which bike is rented and which is not and also
+# which user rented said bike
 class Rent(db.Model):
     __tablename__="rent"
     id = db.Column(db.Numeric, primary_key=True)
@@ -63,6 +75,9 @@ class Rent(db.Model):
 class User(flask_login.UserMixin):
     pass
 
+# Here I setup the verification proccess of the system so that I can find out what
+# kind of user I am dealing with be it a guest, a normal one or an admin
+
 def get_current_user_role():
     Roles = db.session.query(Users).from_statement(text("""SELECT * FROM users where username = '""" + str(flask_login.current_user.id) + """';""")).all()
     for role in Roles:
@@ -76,6 +91,7 @@ def get_current_user_role():
 def error_response():
     return "Error"
 
+# Here I start to restrict access to different users according to their privilages
 def requires_roles(*roles):
     def wrapper(f):
         @wraps(f)
@@ -86,6 +102,8 @@ def requires_roles(*roles):
         return wrapped
     return wrapper
 
+# Here I setup the user login system so that it corresponds accordingly to the Users
+# pass and username
 @login_manager.user_loader
 def user_loader(username):
     usernames = db.session.query(Users.username).filter_by(username=username).scalar() is not None
@@ -96,34 +114,40 @@ def user_loader(username):
     user.id = username
     return user
 
-
+# Here is an error handler if the user doesnt write his/her password or username correctly
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     flash('You need to login first.')
     return redirect(url_for('login'))
 
+# Here I start creating the different routes for the system starting from the default one.
 @app.route('/')
 def hello():
 
     return "Hello World!"
+# This is the login page
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+# This is the page you go to if you fail to login
 @app.route('/incorrect_login')
 def incorrect_login():
     return render_template('loginIncorrect.html')
 
+# This is the register page
 @app.route('/register')
 def register():
     return render_template('register.html')
 
+# This is the admin panel where admins have full control over the user system and bike renting system
 @app.route('/adminPanel')
 @flask_login.login_required
 @requires_roles('admin')
 def adminPanel():
     return render_template('admin_menu.html')
 
+# This is the bike renting page
 @app.route('/rent')
 @flask_login.login_required
 @requires_roles('admin','user')
@@ -131,26 +155,33 @@ def rent():
     flash('Hi ' + flask_login.current_user.id)
     return render_template('rent.html')
 
+# This is the page where you go when you want to return a bike
 @app.route('/unrent')
 @flask_login.login_required
 def unrent():
     return render_template('unrent.html')
 
+# This is the page that shows where the bikes are on a map using the coordinates
+# the bikes have currently
 @app.route('/search')
 @flask_login.login_required
 def search():
         return render_template('mapSearch.html')
 
+# This is the home page of the website
 @app.route('/home')
 @flask_login.login_required
 def home():
     return render_template('home.html')
 
+# This is where bike data is shown as whether they are rented and by whom they are used by.
 @app.route('/showdata')
 def showdata():
     myIotdata=Rent.query.all()
     return render_template('showdata.html', myIotdata=myIotdata)
 
+# Here I make the functionality of the admin panel so that only admins can access it
+# and also have the ability to change other users privilages
 @app.route('/admin_menu', methods=['POST'])
 @flask_login.login_required
 @requires_roles('admin')
@@ -184,6 +215,8 @@ def admin_menu():
 
         return "No such user"
 
+# Here I setup the rent function of the rent page. It's just accessing the table and
+# changing the status of the bike to rented if it is already rented it returns an error
 @app.route('/rent_a_bike', methods=['POST'])
 @flask_login.login_required
 def rent_a_bike():
@@ -200,6 +233,7 @@ def rent_a_bike():
             result = "The Bike is rented by " + user
     return render_template('showRented.html', Rented = result)
 
+# The same as the previous functionality only this one makes them unrented
 @app.route('/unrent_a_bike', methods=['POST'])
 @flask_login.login_required
 def unrent_a_bike():
@@ -214,6 +248,8 @@ def unrent_a_bike():
             result = "The Bike is not rented"
     return render_template('showRented.html', Rented = result)
 
+# Here is the functionality used for searching for a specific bike. It gets the name of the bikes
+# then searches the bike in the Database and returns all it can find about it
 @app.route('/search_bike',  methods=['POST'])
 @flask_login.login_required
 def search_bike():
@@ -221,12 +257,15 @@ def search_bike():
     filter= Bike.query.filter(Bike.name == bike_name)
     return render_template('map.html', myIotdata=filter)
 
+# This functionality gets the last two coordinates of a specific bike.
 @app.route('/last_pos', methods=['POST'])
 @flask_login.login_required
 def last_pos():
     result = db.session.query(Bike).from_statement(text('''SELECT * FROM data  WHERE id IN (SELECT MAX(id) FROM data GROUP BY name);''')).all()
     return render_template('map.html', myIotdata=result)
 
+# This functionality handles the login. It checks with the database if the user exist
+# and has the proper password and username then redirect to the search page.
 @app.route('/login-post', methods=['GET', 'POST'])
 def loginPost():
     users = request.form['username']
@@ -241,17 +280,20 @@ def loginPost():
 
     return 'Bad login'
 
+# This is a simple log out function
 @app.route('/logout')
 @flask_login.login_required
 def logout():
     flask_login.logout_user()
     return 'Logged out'
 
+# This restricts different pages so that only admins and/or users can access them
 @app.route('/protected')
 @flask_login.login_required
 def protected():
     return 'Logged in as: ' + flask_login.current_user.id
 
+# This handles the registration functionality. It simply increments to the already established table
 @app.route('/register',  methods=['POST'])
 def Register():
     userlist = db.session.query(Users.username).filter_by(username=request.form['username']).scalar() is not None
@@ -263,12 +305,14 @@ def Register():
     else:
         return "<h1 style='color: red'>Username is taken</h1>"
 
+# This shows all the bikes on a map.
 @app.route('/map')
 @flask_login.login_required
 def map():
     myIotdata = Bike.query.all()
     return render_template('map.html', myIotdata=myIotdata)
 
+# This protects the file from being accessed from outside files etc.
 if __name__ == '__main__':
 
     app.run(debug=True)
