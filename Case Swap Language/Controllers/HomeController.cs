63e2bc920c0dca21904182a2bd1cd092b04ecb27
Ivaylo Georgiev <ivailo2nd@gmail.com>
2021-04-09 15:41:46 +0200

    Added a new project

---
+++ Case Swap Language/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swap_Language_Case.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swap_Language_Case.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Case Swap Language/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Swap_Language_Case.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
